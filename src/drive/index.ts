662f081a08d258bffdf03bd9ed7cba58ed3dedd9
Nicolas Lima <nichollaslimma734@gmail.com>
2023-05-09 21:01:14 -0300

    feito ajustes

--- src/drive/index.ts
+++ src/drive/index.ts
@@ -1,14 +1,14 @@
 import {google} from "googleapis";
 import { OAuth2Client } from 'google-auth-library';
-import fs from "fs"
 import {Readable } from "stream"
+
+
+
 export const createClient = (client_id, client_secret, redirect_id, refresh_token) => {
     const client = new OAuth2Client(client_id, client_secret, redirect_id);
     client.setCredentials({ refresh_token: refresh_token });
     return google.drive({ version: 'v3', auth: client })
 }
-
-
 
 
 export const createFolder = async (folderName, client) => {
